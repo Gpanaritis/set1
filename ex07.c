c919adb1e6d7521c330678d82de840614ae406ce
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 19:46:34 +0200

    Update ex07.c

--- ex07.c
+++ ex07.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-double d;
-float f;
+  double d;
+  float f;
 
-printf("give double number");
-scanf("%lf",&d);
-f= (float)d;
-printf("double is %.12f and float is %.12f",d,f);
-return 0;
+  printf("give double number");
+  scanf("%lf",&d);
+  f= (float)d;
+  printf("double is %.12f and float is %.12f",d,f);
+  return 0;
 }
