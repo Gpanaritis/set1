ab2fce587cb0e34ca199da845bbd8853beea971d
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 22:59:31 +0200

    Update ex18.c

--- ex18.c
+++ ex18.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-int x,y,result;
-printf("give two numbers\n");
-scanf("%d%d",&x,&y);
-while (x%y!=0 && y%x!=0)
-{
-if (x>y) x=x%y;
-else if (y>x) y=y%x;
-}
-if (x>y) result=y;
-else result=x;
-
-printf( "Max value is : %d\n", result);
-
-return 0;
+  int x,y,result;
+  printf("give two numbers\n");
+  scanf("%d%d",&x,&y);
+  while (x%y!=0 && y%x!=0)
+  {
+    if (x>y) x=x%y;
+    else if (y>x) y=y%x;
+  }
+  if (x>y) result=y;
+  else result=x;
+  
+  printf( "Max value is : %d\n", result);
+  
+  return 0;
 }
