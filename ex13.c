2ecf2526c5558e7003b99464a8a320e12bf2ba6a
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 19:51:04 +0200

    Update ex13.c

--- ex13.c
+++ ex13.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-int a=0,i;
-while (a<=126)
-{
-for(i=1;i<=10;i++) { if (a>126) break; printf("%c(%d)  ",(char)a,a); a++;}
-
-printf("\n");
-}
-return 0;
+  int a=0,i;
+  while (a<=126)
+  {
+    for(i=1;i<=10;i++) { if (a>126) break; printf("%c(%d)  ",(char)a,a); a++;}
+    
+    printf("\n");
+  }
+  return 0;
 }
