ee53c510053fdc5185d6f9a60a86ec6de0ff0f1e
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 22:58:48 +0200

    Update ex17.c

--- ex17.c
+++ ex17.c
@@ -2,23 +2,22 @@
 #include<math.h>
 int main()
 {
-int b[4][10],i,j;
-for (j=1;j<=10;j++)
-{
-b[0][j-1]=j;
-b[1][j-1]=j*2;
-b[2][j-1]=j*3;
-b[3][j-1]=j*10;
-}
-for (j=1;j<=10;j++)
-{
-
-for (i=1;i<=4;i++)
-{
-printf("%d  ", b[i-1][j-1]);
-}
-printf("\n");
-}
-
-return 0;
+  int b[4][10],i,j;
+  for (j=1;j<=10;j++)
+  {
+    b[0][j-1]=j;
+    b[1][j-1]=j*2;
+    b[2][j-1]=j*3;
+    b[3][j-1]=j*10;
+  }
+  for (j=1;j<=10;j++)
+  {
+    for (i=1;i<=4;i++)
+    {
+      printf("%d  ", b[i-1][j-1]);
+    }
+    printf("\n");
+  }
+  
+  return 0;
 }
