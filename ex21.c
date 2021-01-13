c053100c56755bcf03eed339b0e51d433618c676
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 23:36:41 +0200

    Update ex21.c

--- ex21.c
+++ ex21.c
@@ -7,30 +7,30 @@ float positive(float x);
 
 int main()
 {
-int code;
-float time,sms,fpa,total;
-printf("please give your customer code\n");
-scanf("%d",&code);
-while (code<=0 || code/1000==0 || code/1000>9) {printf("please enter a four digit number\n"); scanf("%d",&code);}
-printf("please enter talk time\n");
-time=positive(time);
-printf("please enter amount of messages\n");
-sms=positive(sms);
-time=time*timet;
-sms=sms*smst;
-fpa=(time+sms)*fpat;
-total=time+sms+fpa;
-printf("Tmob account\n");
-printf("---------------------------------------------\n");
-printf("Customer code time  SMS  FPA | TOTAL\n");
-printf("%d          %.1f  %.1f  %.1f | %.3f\n",code,time,sms,fpa,total);
-printf("_____________________________________________");
-return 0;
+  int code;
+  float time,sms,fpa,total;
+  printf("please give your customer code\n");
+  scanf("%d",&code);
+  while (code<=0 || code/1000==0 || code/1000>9) {printf("please enter a four digit number\n"); scanf("%d",&code);}
+  printf("please enter talk time\n");
+  time=positive(time);
+  printf("please enter amount of messages\n");
+  sms=positive(sms);
+  time=time*timet;
+  sms=sms*smst;
+  fpa=(time+sms)*fpat;
+  total=time+sms+fpa;
+  printf("Tmob account\n");
+  printf("---------------------------------------------\n");
+  printf("Customer code time  SMS  FPA | TOTAL\n");
+  printf("%d          %.1f  %.1f  %.1f | %.3f\n",code,time,sms,fpa,total);
+  printf("_____________________________________________");
+  return 0;
 }
 
 float positive(float x)
 {
-scanf("%f",&x);
-while (x<0) {printf("please give positive value\n"); scanf("%f",&x);}
-return x;
+  scanf("%f",&x);
+  while (x<0) {printf("please give positive value\n"); scanf("%f",&x);}
+  return x;
 }
