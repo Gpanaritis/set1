07241b80d8a935ac85b2bcae29f31d99a9ff82a9
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 19:40:40 +0200

    Update ex02.c

--- ex02.c
+++ ex02.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int a=1,i,b=1;
+  int a=1,i,b=1;
 
-while (a<=20)
-{
-for (i=1; i<=5; i++)
-{
-printf("%d ",b);
-b++;
-}
-a++;
-printf("\n");
-}
-return 0;
+  while (a<=20)
+  {
+    for (i=1; i<=5; i++)
+    {
+      printf("%d ",b);
+      b++;
+    }
+    a++;
+    printf("\n");
+   }
+  return 0;
 }
