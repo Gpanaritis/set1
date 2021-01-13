7232a58b5c4c68cd6edff6fa6f69e7c551e8ee7b
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 19:44:03 +0200

    Update ex04.c

--- ex04.c
+++ ex04.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c;
-printf("give two integers \n");
-scanf("%d%d",&a,&b);
-c=a*b;
-if (c>0 && c<10) printf("%d + %d = %d",a,b,a+b);
-else if (c>100 && c<1000) printf("%d - %d = %d",a,b,a-b);
-else printf("not eligible number");
+  int a,b,c;
+  printf("give two integers \n");
+  scanf("%d%d",&a,&b);
+  c=a*b;
+  if (c>0 && c<10) printf("%d + %d = %d",a,b,a+b);
+  else if (c>100 && c<1000) printf("%d - %d = %d",a,b,a-b);
+  else printf("not eligible number");
 }
