3168d70aa5f02ad01d4d0a17f7c239e35d44648f
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 19:51:59 +0200

    Update ex14.c

--- ex14.c
+++ ex14.c
@@ -2,20 +2,20 @@
 #include<math.h>
 int main()
 {
-int N,s=0,i=1;
-
-printf("give an integer ranging 0-10\n");
-scanf("%d",&N);
-
-while (N==0 || i!=N+1)
-{
-if(N>0 && N<10)
-{
-s+=pow(4*i,2);
-i++;
-}
-else{printf("give an integer ranging 0-10\n"); scanf("%d",&N);}
-}
-printf("the expression is %d",s);
-return 0;
+  int N,s=0,i=1;
+  
+  printf("give an integer ranging 0-10\n");
+  scanf("%d",&N);
+  
+  while (N==0 || i!=N+1)
+  {
+    if(N>0 && N<10)
+    {
+      s+=pow(4*i,2);
+      i++;
+    }
+    else{printf("give an integer ranging 0-10\n"); scanf("%d",&N);}
+  }
+  printf("the expression is %d",s);
+  return 0;
 }
