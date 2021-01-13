0fa054a19f586da0f797296e665effbb8e28a4dc
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 19:47:11 +0200

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -3,18 +3,18 @@
 
 int main()
 {
-float ax,bx,by,ay;
-double d;
-printf("give 'a' coordinates \n x=");
-scanf("%f",&ax);
-printf(" y=");
-scanf("%f",&ay);
-printf("give 'b' coordinates \n x=");
-scanf("%f",&bx);
-printf(" y=");
-scanf("%f",&by);
-d=sqrt(pow(ax-bx,2)+pow(ay-by,2));
-printf("distance is %f",d);
+  float ax,bx,by,ay;
+  double d;
+  printf("give 'a' coordinates \n x=");
+  scanf("%f",&ax);
+  printf(" y=");
+  scanf("%f",&ay);
+  printf("give 'b' coordinates \n x=");
+  scanf("%f",&bx);
+  printf(" y=");
+  scanf("%f",&by);
+  d=sqrt(pow(ax-bx,2)+pow(ay-by,2));
+  printf("distance is %f",d);
 
-return 0;
+  return 0;
 }
