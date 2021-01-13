e61111f84a8dd4904e79eb875df4a5d5ce90b70d
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 19:47:43 +0200

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-float F,C;
-printf("give temperature in farenheit \n");
-scanf("%f",&F);
-C=(5/9)*(F-32);
-printf("the temperature is %f degrees celsius",C);
-
-return 0;
+  float F,C;
+  printf("give temperature in farenheit \n");
+  scanf("%f",&F);
+  C=(5/9)*(F-32);
+  printf("the temperature is %f degrees celsius",C);
+  
+  return 0;
 }
